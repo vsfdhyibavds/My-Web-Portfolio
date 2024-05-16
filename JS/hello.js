4266818d25fb5d5586f06f9662918e7bf669b41a
vsfdhyibavds <eugenco578@gmail.com>
2024-05-16 22:24:33 +0300

    dom events

---
+++ JS/hello.js
@@ -0,0 +1,9 @@
+var balance = 1000;
+
+if(balance > 1000){
+    console.log("you can purchase this item");
+} else if(balance < 1000) {
+    console.log("you have insufficient balance");
+} else {
+    console.log("you have just enough.");
+}
