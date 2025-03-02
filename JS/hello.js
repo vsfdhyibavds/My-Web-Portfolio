4077a9f2742eca6fcd2ed50e421465a1b205a026
Eugene Wekesa <eugenco578@gmail.com>
2025-03-03 00:23:48 +0300

    My Web Portfolio

--- JS/hello.js
+++
@@ -1,9 +0,0 @@
-var balance = 1000;
-
-if(balance > 1000){
-    console.log("you can purchase this item");
-} else if(balance < 1000) {
-    console.log("you have insufficient balance");
-} else {
-    console.log("you have just enough.");
-}
